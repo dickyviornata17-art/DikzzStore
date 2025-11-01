906a35fd0580168d0558b4f6ecb1fec16cdc8230
Moh Dikrullah <dickyviornata17@gmail.com>
2025-11-01 13:02:47 +0700

    data Produk js

---
+++ script.js
@@ -0,0 +1,14 @@
+// ====== DATA PRODUK (VERSI LOKAL) ======
+const produkData = [
+  { id: 1, nama: "Baju Muslim Pria Premium", harga: 180000, img: "img/produk1.avif" },
+  { id: 2, nama: "Gamis Muslimah Elegan", harga: 200000, img: "img/produk2.jpg" },
+  { id: 3, nama: "Kaos Distro Streetwear", harga: 120000, img: "img/produk3.jpg" },
+  { id: 4, nama: "Koko Modern Lengan Panjang", harga: 150000, img: "img/produk4.jpg" },
+  { id: 5, nama: "Hijab Casual Harian", harga: 95000, img: "img/produk5.jpg" },
+  { id: 6, nama: "Kaos Oversize Trendy", harga: 85000, img: "img/produk6.webp" },
+  { id: 7, nama: "Sandal Slop Pria", harga: 130000, img: "img/produk7.webp" },
+  { id: 8, nama: "Abaya Dubai Turkey Kaftan", harga: 450000, img: "img/produk8.jpg" },
+  
+];
+
+let keranjang = [];
